a7175eb511d4dd04e6ccfe22d46019eacbe50355
bjones1 <bjones@ece.msstate.edu>
2022-04-27 12:09:10 -0500

    Fix: Migrate transport and client to application service.

--- extensions/IntelliJ/src/main/kotlin/com/github/bjones1/intellij/codechat/services/MyApplicationService.kt
+++ extensions/IntelliJ/src/main/kotlin/com/github/bjones1/intellij/codechat/services/MyApplicationService.kt
@@ -5,12 +5,41 @@
 package com.github.bjones1.intellij.codechat.services
 
 import com.github.bjones1.intellij.codechat.MyBundle
+import com.github.bjones1.intellij.codechat.gen_java.CodeChat_ServicesConstants
+import com.github.bjones1.intellij.codechat.gen_java.EditorPlugin
+import com.intellij.ide.util.PropertiesComponent
+import com.intellij.openapi.Disposable
 import com.intellij.openapi.components.Service
+import org.apache.thrift.protocol.TBinaryProtocol
+import org.apache.thrift.protocol.TProtocol
+import org.apache.thrift.transport.TSocket
 
 @Service
-class MyApplicationService {
+class MyApplicationService: Disposable {
+    private var transport: TSocket? = null
+    var client: EditorPlugin.Client? = null
+        private set
+        get() {
+            if (transport == null) {
+                // Start the server, in case it's not running. First, get the path to the server's executable.
+                val codeChatServerExe = PropertiesComponent.getInstance()
+                    .getValue("com.github.bjones1.intellij.codechat.CodeChatBinaryPath", "CodeChat_Server")
+                println(codeChatServerExe)
+                // TODO: Run the server.
+
+                // Connect to the server. See Thrift's `Java tutorial <https://thrift.apache.org/tutorial/java.html>`_ and the corresponding `tutorial files on GitHub <https://github.com/apache/thrift/tree/master/lib/java>`_.
+                transport = TSocket("localhost", CodeChat_ServicesConstants.THRIFT_PORT.toInt())
+                transport!!.open()
+            }
+            val protocol: TProtocol = TBinaryProtocol(transport)
+            return EditorPlugin.Client(protocol)
+        }
 
     init {
         println(MyBundle.message("applicationService"))
+    }
+
+    override fun dispose() {
+        transport?.close()
     }
 }
--- extensions/IntelliJ/src/main/kotlin/com/github/bjones1/intellij/codechat/services/MyProjectService.kt
+++ extensions/IntelliJ/src/main/kotlin/com/github/bjones1/intellij/codechat/services/MyProjectService.kt
@@ -7,13 +7,8 @@ package com.github.bjones1.intellij.codechat.services
 import com.github.bjones1.intellij.codechat.MyBundle
 import com.intellij.openapi.project.Project
 import org.apache.thrift.TException
-import org.apache.thrift.protocol.TBinaryProtocol
-import org.apache.thrift.protocol.TProtocol
-import org.apache.thrift.transport.TSocket
-import org.apache.thrift.transport.TTransport
-import com.github.bjones1.intellij.codechat.gen_java.CodeChat_ServicesConstants.THRIFT_PORT
-import com.github.bjones1.intellij.codechat.gen_java.EditorPlugin
 import com.intellij.openapi.components.Service
+import com.intellij.openapi.components.service
 
 @Service
 class MyProjectService(project: Project) {
@@ -22,14 +17,8 @@ class MyProjectService(project: Project) {
         println(MyBundle.message("projectService", project.name))
 
         try {
-            // See Thrift's `Java tutorial <https://thrift.apache.org/tutorial/java.html>`_ and the corresponding `tutorial files on GitHub <https://github.com/apache/thrift/tree/master/lib/java>`_.
-            val transport: TTransport
-            transport = TSocket("localhost", THRIFT_PORT.toInt())
-            transport.open()
-            val protocol: TProtocol = TBinaryProtocol(transport)
-            val client: EditorPlugin.Client = EditorPlugin.Client(protocol)
-            client.ping()
-            transport.close()
+            val applicationService = service<MyApplicationService>()
+            applicationService.client!!.ping()
         } catch (x: TException) {
             x.printStackTrace()
         }
